a5a4169e9a940dd56f051338325435214de800f2
fierzahaikkal <fierzahf.gdscug@gmail.com>
2025-01-10 20:51:21 +0700

    fix: fixing deps issues

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,25 @@
 module.exports = function (api) {
   api.cache(true);
-  const plugins = [];
-
-  plugins.push([
-    '@tamagui/babel-plugin',
-    {
-      components: ['tamagui'],
-      config: './tamagui.config.ts',
-    },
-  ]);
-
   return {
     presets: ['babel-preset-expo'],
-
-    plugins,
+    plugins: [
+      // Add any additional babel plugins you need
+      'react-native-reanimated/plugin',
+      [
+        '@tamagui/babel-plugin',
+        {
+          components: ['tamagui'],
+          config: './tamagui.config.ts',
+        },
+      ],
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@': './src',
+          },
+        },
+      ],
+    ],
   };
 };
